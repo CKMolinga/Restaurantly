e85012ee07612399cfc15c80ab450c1afdf54293
CKMolinga <ck.molinga@gmail.com>
2022-08-07 22:34:23 +0200

    initial commit

---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Page Not Found</h1>
+      <NuxtLink to="/">Go Home</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Welcome To Restaurantly</h1>
+      <NuxtLink to="/restaurants">Explore Restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
